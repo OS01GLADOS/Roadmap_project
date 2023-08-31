a91948de9603b0bc8c2390e517b7ee3720062516
d.s <no.name.tyan.d@gmail.com>
2023-08-31 12:29:40 +0300

    add black and hooks

--- new_blog_app/roadmapsite/wsgi.py
+++ new_blog_app/roadmapsite/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'roadmapsite.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "roadmapsite.settings")
 
 application = get_wsgi_application()
