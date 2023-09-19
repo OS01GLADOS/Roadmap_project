993fe88ea0f99bb9238c74035a5e1b7b140c3c04
d.s <no.name.tyan.d@gmail.com>
2023-09-19 15:49:23 +0300

    almost finish with postgres, without backups

--- new_blog_app/roadmapsite/settings.py
+++ new_blog_app/roadmapsite/settings.py
@@ -103,9 +103,18 @@ WSGI_APPLICATION = "roadmapsite.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # },
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "OPTIONS": {"options": "-c search_path=custom_scheme"},
+        "NAME": "new_db",
+        "USER": "djangosite",
+        "PASSWORD": "1111",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
